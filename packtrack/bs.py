12914bb8d843b558ed857a9f7e577646dd319457
Iuri de Silvio <iurisilvio@gmail.com>
2019-05-08 17:27:07 -0300

    .

--- packtrack/bs.py
+++ packtrack/bs.py
@@ -2,8 +2,8 @@ import functools
 
 try:
     from bs4 import BeautifulSoup
+    BeautifulSoup = functools.partial(BeautifulSoup, features="lxml")
 except ImportError:
     from BeautifulSoup import BeautifulSoup
-    BeautifulSoup = functools.partial(BeautifulSoup, features="lxml")
 
 BeautifulSoup = BeautifulSoup
